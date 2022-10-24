a116617c18e27fd8ce574c5c72d90f3b29dd47d4
Paul Thomas <paulthomas2107@gmail.com>
2022-10-24 22:41:08 +0100

    More....

--- app.js
+++ app.js
@@ -1,40 +1,73 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
-
+const playText = document.getElementById('playText');
 const spaces = [null, null, null, null, null, null, null, null, null];
 
-const O_TEXT = "O";
-const X_TEXT = "X";
+const O_TEXT = 'O';
+const X_TEXT = 'X';
 
 let currentPlayer = O_TEXT;
 
- 
 const drawBoard = () => {
-    boxes.forEach((box, index) => {
-        let styleString = '';
-        if (index < 3) {
-            styleString += `border-bottom: 3px solid var(--purple);`;
-        } 
-        if (index % 3 === 0) {
-            styleString += `border-right: 3px solid var(--purple);`;
-        }
-        if (index % 3 === 2) {
-            styleString += `border-left: 3px solid var(--purple);`;
-        }
-        if (index > 5) {
-            styleString += `border-top: 3px solid var(--purple);`;
-        }
-        box.style = styleString;
-        box.addEventListener('click', boxClicked)
-    })
-}
+  boxes.forEach((box, index) => {
+    let styleString = '';
+    if (index < 3) {
+      styleString += `border-bottom: 3px solid var(--purple);`;
+    }
+    if (index % 3 === 0) {
+      styleString += `border-right: 3px solid var(--purple);`;
+    }
+    if (index % 3 === 2) {
+      styleString += `border-left: 3px solid var(--purple);`;
+    }
+    if (index > 5) {
+      styleString += `border-top: 3px solid var(--purple);`;
+    }
+    box.style = styleString;
+    box.addEventListener('click', boxClicked);
+  });
+};
 
 const boxClicked = (e) => {
-    const id = e.target.id;
-    if (!spaces[id]) {
-        spaces[id] = currentPlayer;
-        e.target.innerText =  currentPlayer;
-        currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
+  const id = e.target.id;
+  if (!spaces[id]) {
+    spaces[id] = currentPlayer;
+    e.target.innerText = currentPlayer;
+    if (playerHasWon()) {
+      playText.innerText = `${currentPlayer} has won !`;
+      return;
     }
-}
+    currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
+  }
+};
+
+const playerHasWon = () => {
+    // One
+  if (spaces[0] === currentPlayer) {
+    if (spaces[0] === currentPlayer && spaces[2] === currentPlayer) {
+      console.log(`${currentPlayer} wins up top`);
+      return true;
+    }
+    if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+      console.log(`${currentPlayer} wins on left`);
+      return true;
+    }
+    if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+      console.log(`${currentPlayer} wins diagonally`);
+      return true;
+    }
+  }
+  // Two
+  if (spaces[8] === currentPlayer) {
+    if (spaces[2] === currentPlayer && spaces[5] === currentPlayer) {
+      console.log(`${currentPlayer} wins up right`);
+      return true;
+    }
+    if (spaces[6] === currentPlayer && spaces[7] === currentPlayer) {
+      console.log(`${currentPlayer} wins on bottom`);
+      return true;
+    }
+  }
+  // Three
+};
 
 drawBoard();
